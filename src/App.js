58ffcd920a90acbc13e9629d6ae3b92daaea7193
AlvGonzalez <alvarocgonzalezmaldonado@gmail.com>
2023-01-17 19:54:28 +0100

    Breadcrumb is created

--- src/App.js
+++ src/App.js
@@ -1,53 +1,26 @@
 import './App.css';
 import { ChakraProvider, 
-  theme, 
-  Box,
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon, } from '@chakra-ui/react'
-
+  theme, Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator } from '@chakra-ui/react'
+import Tittle from './Components/Tittle/tittle.js'
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
     <div className="App">
-    <Accordion>
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left'>
-          Section 1 title
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-  </AccordionItem>
+      <Tittle name = "Adopciones"/>
+      <Breadcrumb separator='-'>
+        <BreadcrumbItem>
+          <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+        </BreadcrumbItem>
 
-  <AccordionItem>
-    <h2>
-      <AccordionButton>
-        <Box as="span" flex='1' textAlign='left'>
-          Section 2 title
-        </Box>
-        <AccordionIcon />
-      </AccordionButton>
-    </h2>
-    <AccordionPanel pb={4}>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat.
-    </AccordionPanel>
-  </AccordionItem>
-</Accordion>
+        <BreadcrumbItem>
+          <BreadcrumbLink href='pets'>Mascotas</BreadcrumbLink>
+        </BreadcrumbItem>
+
+        <BreadcrumbItem isCurrentPage>
+          <BreadcrumbLink href='myuser'>Mi Sitio</BreadcrumbLink>
+        </BreadcrumbItem>
+</Breadcrumb>
     </div>
     </ChakraProvider>
   );
