0780908a6761770d20fa7b39619841932388fc36
AlvGonzalez <alvarocgonzalezmaldonado@gmail.com>
2023-01-20 20:04:42 +0100

    menu is updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { ChakraProvider,Button, ButtonGroup } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import Tittle from './Components/Tittle/tittle.js'
 import Menu from './Components/Menu/menu.js'
 import Parrilla from './Components/Parrilla/parrilla.js'
--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -1,27 +1,30 @@
 import React from "react";
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, ButtonGroup, Button } from '@chakra-ui/react'
-import {styles} from "./menu.css"
+import styles from "./menu.css"
 
 const Menu = () => {
     return <div> 
         
-        <Breadcrumb separator='-' className="menu1" display="flex" justifyContent="space-around" fontFamily='Jaldi' paddingLeft={0} paddingBottom={2}>
-            <BreadcrumbItem isCurrentPage className="menu2">
-                <BreadcrumbLink href='#' className="menu3">Inicio</BreadcrumbLink>
+        <Breadcrumb separator='-' className="menu1" fontFamily='Jaldi' paddingLeft={0} paddingBottom={2} >
+            <div className="AAA">
+            <BreadcrumbItem isCurrentPage className="menu2" padding={4} >
+                <BreadcrumbLink href='caca' className="menu3">Inicio</BreadcrumbLink>
             </BreadcrumbItem>
 
-            <BreadcrumbItem className="menu2">
+            <BreadcrumbItem className="menu2" padding={4}>
                <BreadcrumbLink href='pets' className="menu3" >Mascotas</BreadcrumbLink>
             </BreadcrumbItem>
 
-            <BreadcrumbItem className="menu2">
-                <BreadcrumbLink href='myuser' className="menu3">Mi Perfil</BreadcrumbLink>
+            <BreadcrumbItem className="menu2" padding={4} colorScheme='yellow'>
+                <BreadcrumbLink href='myuser' className="menu3" >Mi Perfil</BreadcrumbLink>
             </BreadcrumbItem>
+            </div>
 
-            <ButtonGroup spacing='2' >
-                <Button className="botonficha" variant='solid' colorScheme='yellow' color='white' bg='#f23084'> Log-in </Button>
+            <div className="BBB">            
+            <ButtonGroup spacing='2' paddingRight={4}>
+                <Button className="botonficha" variant='solid' colorScheme='yellow' color='white' bg='#f23084'  > Log-in </Button>
             </ButtonGroup>
-
+            </div>
         </Breadcrumb>
         </div>
 }
