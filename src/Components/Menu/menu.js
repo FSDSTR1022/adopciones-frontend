30c67ba821fbb5c5c33b80928066ba894dd703cf
AlvGonzalez <alvarocgonzalezmaldonado@gmail.com>
2023-01-20 17:48:32 +0100

    estructura de la web en front

--- src/Components/Menu/menu.js
+++ src/Components/Menu/menu.js
@@ -1,21 +1,25 @@
 import React from "react";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, ButtonGroup, Button } from '@chakra-ui/react'
 import {styles} from "./menu.css"
 
 const Menu = () => {
     return <div> 
-        <Breadcrumb separator='  -' className="menu1">
-            <BreadcrumbItem isCurrentPage m={2} p={2} className="menu2">
+        
+        <Breadcrumb separator='-' className="menu1" display="flex" justifyContent="space-between" fontFamily='Jaldi' paddingLeft={5} paddingBottom={2}>
+            <BreadcrumbItem isCurrentPage className="menu2">
                 <BreadcrumbLink href='#' className="menu3">Inicio</BreadcrumbLink>
             </BreadcrumbItem>
 
-            <BreadcrumbItem m={2} p={2} className="menu2">
-               <BreadcrumbLink href='pets' className="menu3">Mascotas</BreadcrumbLink>
+            <BreadcrumbItem className="menu2">
+               <BreadcrumbLink href='pets' className="menu3" >Mascotas</BreadcrumbLink>
             </BreadcrumbItem>
 
-            <BreadcrumbItem m={2} p={2}  className="menu2">
+            <BreadcrumbItem className="menu2">
                 <BreadcrumbLink href='myuser' className="menu3">Mi Perfil</BreadcrumbLink>
             </BreadcrumbItem>
+            <ButtonGroup spacing='2' >
+                <Button className="botonficha" variant='solid' colorScheme='yellow' color='white' bg='#f23084'> Log-in </Button>
+            </ButtonGroup>
         </Breadcrumb>
         </div>
 }
